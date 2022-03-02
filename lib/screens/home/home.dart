f2e1d99fee03418800cdad53e17cf42fa90e6e9f
bbobohang <bbobohang@gmail.com>
2022-03-02 16:17:31 +0800

    Added bottom navigation bar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,21 +8,40 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         child: Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text('Carpark Home'),
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          ElevatedButton.icon(
-            icon: Icon(Icons.person),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-            label: Text('Log Out'),
-          )
-        ],
-      ),
-    ));
+            backgroundColor: Colors.blue[50],
+            appBar: AppBar(
+              title: Text('Carpark Home'),
+              backgroundColor: Colors.blue[400],
+              elevation: 0.0,
+              actions: <Widget>[
+                ElevatedButton.icon(
+                  icon: Icon(Icons.person),
+                  onPressed: () async {
+                    await _auth.signOut();
+                  },
+                  label: Text('Log Out'),
+                )
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                      backgroundColor: Colors.blue),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.star),
+                      label: 'Favourites',
+                      backgroundColor: Colors.blue),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.car_rental),
+                      label: 'Lot Remember',
+                      backgroundColor: Colors.blue),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.help),
+                      label: 'Help',
+                      backgroundColor: Colors.blue),
+                ])));
   }
 }
