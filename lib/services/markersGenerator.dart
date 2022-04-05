8a347b5c434652a8194fc3ce1eb56258c5198969
cheenngg <chloeehengg@gmail.com>
2022-04-06 00:54:55 +0800

    filters code
    filters and markergenerator edited

--- lib/services/markersGenerator.dart
+++ lib/services/markersGenerator.dart
@@ -7,7 +7,7 @@ import 'package:flutter_application_2/main.dart' as globals;
 
 class markersGenerator extends StatefulWidget {
   const markersGenerator({Key? key}) : super(key: key);
-  void generate_marker_set(
+  void generate_low_marker_set(
       lat,
       long,
       address,
@@ -97,16 +97,189 @@ class markersGenerator extends StatefulWidget {
             })));
   }
 
-  @override
-  State<markersGenerator> createState() => _markersGeneratorState();
-}
+  void generate_mid_marker_set(
+      lat,
+      long,
+      address,
+      id,
+      vacancy,
+      carpark_basement,
+      carpark_decks,
+      carpark_no,
+      carpark_type,
+      free_parking,
+      gantry_height,
+      night_parking,
+      short_term_parking,
+      type_of_parking_system,
+      context) {
+    print("adding " + lat.toString() + " " + long.toString());
+    //list of markers
+    globals.markers.add(Marker(
+        markerId: MarkerId(id.toString()),
+        position: LatLng(lat, long),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow),
+        infoWindow: InfoWindow(
+            //popup info
+            title: address,
+            snippet: 'ID: ' + id.toString(),
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return ListView(
+                      children: <Widget>[
+                        Card(
+                            child: ListTile(
+                          leading: const Icon(Icons.location_pin),
+                          contentPadding: EdgeInsets.all(7.0),
+                          title:
+                              Text(address, style: TextStyle(fontSize: 18.0)),
+                        )),
+                        Card(
+                            child: ListTile(
+                          leading: const Icon(Icons.credit_card),
+                          contentPadding: EdgeInsets.all(7.0),
+                          title: Text("ID : " + id.toString(),
+                              style: TextStyle(fontSize: 18.0)),
+                        )),
+                        Card(
+                            child: ListTile(
+                          leading: const Icon(Icons.car_rental),
+                          contentPadding: EdgeInsets.all(7.0),
+                          title: Text("Availability: " + vacancy.toString(),
+                              style: TextStyle(fontSize: 18.0)),
+                        )),
+                        SizedBox(height: 20.0),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 20, 27, 66),
+                            minimumSize: Size(25, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                          ),
+                          child: Text("View full details",
+                              style: TextStyle(fontSize: 20.0)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => FullDetails(
+                                          id,
+                                          address,
+                                          carpark_basement,
+                                          carpark_decks,
+                                          carpark_no,
+                                          carpark_type,
+                                          free_parking,
+                                          gantry_height,
+                                          night_parking,
+                                          short_term_parking,
+                                          type_of_parking_system,
+                                          lat,
+                                          long,
+                                        )));
+                          },
+                        )
+                      ],
+                    );
+                  });
+            })));
+  }
 
-class _markersGeneratorState extends State<markersGenerator> {
-  @override
-  Widget build(BuildContext context) {
-    int x = 0;
-    for (carparkDetail objects in globals.carparkObjects) {
-      widget.generate_marker_set(
+  void generate_high_marker_set(
+      lat,
+      long,
+      address,
+      id,
+      vacancy,
+      carpark_basement,
+      carpark_decks,
+      carpark_no,
+      carpark_type,
+      free_parking,
+      gantry_height,
+      night_parking,
+      short_term_parking,
+      type_of_parking_system,
+      context) {
+    print("adding " + lat.toString() + " " + long.toString());
+    //list of markers
+    globals.markers.add(Marker(
+        markerId: MarkerId(id.toString()),
+        position: LatLng(lat, long),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        infoWindow: InfoWindow(
+            //popup info
+            title: address,
+            snippet: 'ID: ' + id.toString(),
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return ListView(
+                      children: <Widget>[
+                        Card(
+                            child: ListTile(
+                          leading: const Icon(Icons.location_pin),
+                          contentPadding: EdgeInsets.all(7.0),
+                          title:
+                              Text(address, style: TextStyle(fontSize: 18.0)),
+                        )),
+                        Card(
+                            child: ListTile(
+                          leading: const Icon(Icons.credit_card),
+                          contentPadding: EdgeInsets.all(7.0),
+                          title: Text("ID : " + id.toString(),
+                              style: TextStyle(fontSize: 18.0)),
+                        )),
+                        Card(
+                            child: ListTile(
+                          leading: const Icon(Icons.car_rental),
+                          contentPadding: EdgeInsets.all(7.0),
+                          title: Text("Availability: " + vacancy.toString(),
+                              style: TextStyle(fontSize: 18.0)),
+                        )),
+                        SizedBox(height: 20.0),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 20, 27, 66),
+                            minimumSize: Size(25, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                          ),
+                          child: Text("View full details",
+                              style: TextStyle(fontSize: 20.0)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => FullDetails(
+                                          id,
+                                          address,
+                                          carpark_basement,
+                                          carpark_decks,
+                                          carpark_no,
+                                          carpark_type,
+                                          free_parking,
+                                          gantry_height,
+                                          night_parking,
+                                          short_term_parking,
+                                          type_of_parking_system,
+                                          lat,
+                                          long,
+                                        )));
+                          },
+                        )
+                      ],
+                    );
+                  });
+            })));
+  }
+
+  void generate_colored_markers(carparkDetail objects, BuildContext context) {
+    if (objects.vacancy >= 1 && objects.vacancy < 33) {
+      generate_low_marker_set(
           objects.lat,
           objects.long,
           objects.address,
@@ -122,9 +295,62 @@ class _markersGeneratorState extends State<markersGenerator> {
           objects.short_term_parking,
           objects.type_of_parking_system,
           context);
-      x++;
+    } else if (objects.vacancy >= 33 && objects.vacancy < 66) {
+      generate_mid_marker_set(
+          objects.lat,
+          objects.long,
+          objects.address,
+          objects.id,
+          objects.vacancy,
+          objects.carpark_basement,
+          objects.carpark_decks,
+          objects.carpark_no,
+          objects.carpark_type,
+          objects.free_parking,
+          objects.gantry_height,
+          objects.night_parking,
+          objects.short_term_parking,
+          objects.type_of_parking_system,
+          context);
+    } else {
+      generate_high_marker_set(
+          objects.lat,
+          objects.long,
+          objects.address,
+          objects.id,
+          objects.vacancy,
+          objects.carpark_basement,
+          objects.carpark_decks,
+          objects.carpark_no,
+          objects.carpark_type,
+          objects.free_parking,
+          objects.gantry_height,
+          objects.night_parking,
+          objects.short_term_parking,
+          objects.type_of_parking_system,
+          context);
     }
-    print("Markers created: " + x.toString());
-    return landingMap();
+  }
+
+  @override
+  State<markersGenerator> createState() => _markersGeneratorState();
+}
+
+class _markersGeneratorState extends State<markersGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    if (globals.filterState == false) {
+      int x = 0;
+      for (carparkDetail objects in globals.carparkObjects) {
+        widget.generate_colored_markers(objects, context);
+        x++;
+      }
+
+      globals.markersFiltered = globals.markers;
+      print("Markers created: " + x.toString());
+      return landingMap();
+    } else {
+      return landingMap();
+    }
   }
 }
