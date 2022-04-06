6c40d2b0fb3c6a25f07a9b7a96a165b027b311ef
michellelsa <michelleee1617@gmail.com>
2022-04-06 19:54:16 +0800

    Working
    7.55pm

--- lib/screens/FullDetails.dart
+++ lib/screens/FullDetails.dart
@@ -6,7 +6,6 @@ import 'package:flutter_application_2/screens/Favourites.dart' as global;
 import 'package:flutter_application_2/screens/home/home.dart';
 import 'package:get/get.dart';
 
-
 class FullDetails extends StatefulWidget {
   // const FullDetails({ Key? key }) : super(key: key);
   late String _id;
@@ -54,9 +53,9 @@ class FullDetails extends StatefulWidget {
     this._type_of_parking_system = type_of_parking_system;
     this.x_coord = x_cord;
     this.y_coord = y_cord;
-    if (global.favourited.contains(_address)){
+    if (global.favourited.contains(_address)) {
       this.favourite = true;
-    }else{
+    } else {
       this.favourite = false;
     }
   }
@@ -66,90 +65,100 @@ class FullDetails extends StatefulWidget {
 }
 
 class _FullDetailsState extends State<FullDetails> {
-
   @override
   Widget build(BuildContext context) {
-      print(widget.favourite);
-      return Scaffold(
-          appBar: AppBar(
-            title: Text("Full Details"),
-            backgroundColor: Color.fromARGB(255, 20, 27, 66),
-            actions: <Widget>[
-                FavoriteButton(
-                  isFavorite: widget.favourite,
-                  valueChanged: (_isFavorite) {
-                    if(_isFavorite){
-                      print('Is Favorite : $_isFavorite');
-                      setState(() {
-                        global.myFavourites.add(widget._address);
-                        global.favourited.add(widget._address);
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => MyBottomNavigatioBar()));
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => global.favouritePage()));
-                      });
-                    }else{
-                      print('Is Favorite : $_isFavorite');
-                      setState(() {
-                        global.myFavourites.remove(widget._address);
-                        global.favourited.remove(widget._address);
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => MyBottomNavigatioBar()));
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => global.favouritePage()));
-                      });
-                    }
+    print(widget.favourite);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Full Details"),
+          backgroundColor: Color.fromARGB(255, 20, 27, 66),
+          actions: <Widget>[
+            FavoriteButton(
+                isFavorite: widget.favourite,
+                valueChanged: (_isFavorite) {
+                  if (_isFavorite) {
+                    print('Is Favorite : $_isFavorite');
+                    setState(() {
+                      global.myFavourites.add(widget._address);
+                      global.favourited.add(widget._address);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyBottomNavigatioBar()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => global.favouritePage()));
+                    });
+                  } else {
+                    print('Is Favorite : $_isFavorite');
+                    setState(() {
+                      global.myFavourites.remove(widget._address);
+                      global.favourited.remove(widget._address);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyBottomNavigatioBar()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => global.favouritePage()));
+                    });
                   }
-                ),
-            ],
-          ),
-          body: ListView(
-            children: [
-              Card(
-                  elevation: 5.0,
-                  color: Color.fromARGB(255, 177, 195, 216),
-                  child: ListTile(
-                    leading: Icon(Icons.location_pin, size: 28.0),
-                    title: Text(
-                      widget._address,
-                      textAlign: TextAlign.justify,
-                    ),
-                  )),
-              Card(
-                  elevation: 5.0,
-                  child: ListTile(
-                      leading: Icon(Icons.car_rental, size: 30.0),
-                      title: Text('\nCarpark Information\n\n'),
-                      subtitle: Text(
-                          'Carpark Type: ' +
-                              widget._carpark_type +
-                              '\n\n' +
-                              'Carpark Basement: ' +
-                              widget._carpark_basement +
-                              '\n\n' +
-                              'Gantry Height: ' +
-                              widget._gantry_height +
-                              '\n',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontSize: 15.0)))),
-              Card(
-                  elevation: 5.0,
-                  child: ListTile(
-                      leading: Icon(Icons.local_parking, size: 28.0),
-                      title: Text('\nParking Information\n\n'),
-                      subtitle: Text(
-                          'Free Parking: ' +
-                              widget._free_parking +
-                              '\n\n' +
-                              'Short Term Parking: ' +
-                              widget._short_term_parking +
-                              '\n\n' +
-                              'Night Parking: ' +
-                              widget._night_parking +
-                              '\n\n' +
-                              'Parking System: ' +
-                              widget._type_of_parking_system +
-                              '\n',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontSize: 15.0))))
-            ],
-            shrinkWrap: true,
-          ));
+                }),
+          ],
+        ),
+        body: ListView(
+          children: [
+            Card(
+                elevation: 5.0,
+                color: Color.fromARGB(255, 177, 195, 216),
+                child: ListTile(
+                  leading: Icon(Icons.location_pin, size: 28.0),
+                  title: Text(
+                    widget._address,
+                    textAlign: TextAlign.justify,
+                  ),
+                )),
+            Card(
+                elevation: 5.0,
+                child: ListTile(
+                    leading: Icon(Icons.car_rental, size: 30.0),
+                    title: Text('\nCarpark Information\n\n'),
+                    subtitle: Text(
+                        'Carpark Type: ' +
+                            widget._carpark_type +
+                            '\n\n' +
+                            'Carpark Basement: ' +
+                            widget._carpark_basement +
+                            '\n\n' +
+                            'Gantry Height: ' +
+                            widget._gantry_height +
+                            '\n',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(fontSize: 15.0)))),
+            Card(
+                elevation: 5.0,
+                child: ListTile(
+                    leading: Icon(Icons.local_parking, size: 28.0),
+                    title: Text('\nParking Information\n\n'),
+                    subtitle: Text(
+                        'Free Parking: ' +
+                            widget._free_parking +
+                            '\n\n' +
+                            'Short Term Parking: ' +
+                            widget._short_term_parking +
+                            '\n\n' +
+                            'Night Parking: ' +
+                            widget._night_parking +
+                            '\n\n' +
+                            'Parking System: ' +
+                            widget._type_of_parking_system +
+                            '\n',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(fontSize: 15.0))))
+          ],
+          shrinkWrap: true,
+        ));
   }
 }
